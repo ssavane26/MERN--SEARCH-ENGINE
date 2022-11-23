0b34ff8dc9c5a71052448eba0887187ff7a3b979
ssavane26 <savaneprod@gmail.com>
2022-11-22 19:24:56 -0500

    removed routes require and added middlewware require for now

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
