b6060c35c160550002e6ba1d8b44e0229503cf99
ssavane26 <savaneprod@gmail.com>
2022-12-01 22:03:19 -0500

    update

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ import Navbar from './components/Navbar';
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: '/graphql',
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
